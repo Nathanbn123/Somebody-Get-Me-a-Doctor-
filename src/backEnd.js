49050c11ab7a2d06055ade78c8d344f6f8cdb994
Nathan Nielson <nathanbn123@gmail.com>
2019-03-22 08:44:16 -0700

    create initial files and setup

---
+++ src/backEnd.js
@@ -0,0 +1,76 @@
+import $ from 'jquery';
+
+
+export class Doctor {
+  constructor() {
+
+  }
+
+  findDoc(){
+    const that = this;
+    const promise = new Promise(function(resolve, reject) {
+      const request = new XMLHttpRequest();
+      let url = `http://dinoipsum.herokuapp.com/api/?format=json`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      that.data = body;
+    }, function(error) {
+      console.log(error);
+    });
+  }
+
+
+
+
+
+
+//
+//  export class Dino {
+//    constructor() {
+//     this.data = [];
+//    }
+//
+//    addToArr(data) {
+//
+//      alert(data);
+//      this.data = data;
+//      // console.log(this.data);
+//    }
+//   summonDinos() {
+//
+//    const getDinos = $.get('http://dinoipsum.herokuapp.com/api/?format=json');
+//    getDinos.then(function(response){
+//      // console.log(response);
+//      // this.singleDino = response;
+//      // console.log(this.singleDino[1][2]);
+//      this.addToArr(response);
+//    });
+//   }
+//
+//
+//   fillContainer(html) {
+//     $('.dino-output').html(html);
+//   };
+//   oops() {
+//      console.log('Where did all the dinosaurs go?');
+//   };
+// }
+
+  // getArr(arr) {
+  //   const dinoArr = arr[0];
+  //   console.log(dinoArr[0]);
+  // }
+
+ // const testTest= new Dino;
+ // console.log(testTest.summonDinos())
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import 'bootstrap';
+import 'bootstrap/scss/bootstrap.scss';
+import './sass/styles.scss';
+import $ from 'jquery';
+import {Doctor} from "./backEnd.js";
+
+
+
+$(document).ready(function() {
+
+
+});
